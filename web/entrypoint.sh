8f342f208f775f6bbc161b653dc1a7f891945319
Ang <federico.ang@p84487.intra.rakuten.co.jp>
2021-06-08 09:54:34 +0900

    Updated dependencies, nginx arguments fix.

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -16,7 +16,7 @@ echo 'server {
     listen 80 default_server;
     add_header 'Access-Control-Allow-Origin' '*';
     add_header 'Access-Control-Allow-Credentials' 'true';
-    add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+    add_header 'Access-Control-Allow-Methods' 'GET,POST,OPTIONS';
     add_header 'Access-Control-Allow-Headers' 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type';
     location / {
         try_files $uri @app;
